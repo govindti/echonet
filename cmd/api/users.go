9f54a5b28f80e308259d782a03cfc14e9249a45e
govindti <108tiwari.g@gmail.com>
2026-03-15 23:53:10 +0530

    Implement follow and unfollow functionality with context middleware for user management

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"strconv"
 
@@ -8,27 +9,91 @@ import (
 	"github.com/govindti/echonet/internal/store"
 )
 
+type userKey string
+
+const userCtx userKey = "user"
+
 func (app *Application) getUserHandler(w http.ResponseWriter, r *http.Request) {
-	userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
-	if err != nil || userID < 1 {
+	user := getUserFromContext(r)
+
+	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
+
+type FollowUser struct {
+	ID int64 `json:"user_id,string"`
+}
+
+func (app *Application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+	followedUser := getUserFromContext(r)
+	// ToDo: Revert back to auth userID from ctx
+	var payload FollowUser
+	if err := readJSON(w, r, &payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
 
-	ctx := r.Context()
-
-	user, err := app.store.Users.GetByID(ctx, userID)
-	if err != nil {
-		switch err {
-		case store.ErrNotFound:
-			app.notFoundResponse(w, r, err)
-			return
+	if err := app.store.Followers.Follow(r.Context(), followedUser.ID, payload.ID); err != nil {
+		switch err{
+		case store.ErrConflict:
+			app.conflictResponse(w, r, err)
+			return 
 		default:
 			app.internalServerError(w, r, err)
 			return
 		}
 	}
-	if err := app.jsonResponse(w, http.StatusOK, user); err != nil {
+	if err := app.jsonResponse(w, http.StatusNoContent, nil); err != nil {
 		app.internalServerError(w, r, err)
 	}
+}
+
+func (app *Application) unfollowUserHandler(w http.ResponseWriter, r *http.Request) {
+	followedUser := getUserFromContext(r)
+	// ToDo: Revert back to auth userID from ctx
+	var payload FollowUser
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := app.store.Followers.Unfollow(r.Context(), followedUser.ID, payload.ID); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+	if err := app.jsonResponse(w, http.StatusNoContent, nil); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
+
+func (app *Application) usersContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userID, err := strconv.ParseInt(chi.URLParam(r, "userID"), 10, 64)
+		if err != nil || userID < 1 {
+			app.badRequestResponse(w, r, err)
+			return
+		}
+
+		ctx := r.Context()
+		user, err := app.store.Users.GetByID(ctx, userID)
+		if err != nil {
+			switch err {
+			case store.ErrNotFound:
+				app.notFoundResponse(w, r, err)
+				return
+			default:
+				app.internalServerError(w, r, err)
+				return
+			}
+		}
+
+		ctx = context.WithValue(ctx, userCtx, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getUserFromContext(r *http.Request) *store.User {
+	user, _ := r.Context().Value(userCtx).(*store.User)
+	return user
 }
--- internal/store/storage.go
+++ internal/store/storage.go
@@ -8,8 +8,9 @@ import (
 )
 
 var (
-	ErrNotFound = errors.New("resource not found")
+	ErrNotFound          = errors.New("resource not found")
 	QueryTimeoutDuration = time.Second * 5
+	ErrConflict          = errors.New("resource already exists")
 )
 
 type Storage struct {
@@ -22,18 +23,22 @@ type Storage struct {
 	Users interface {
 		Create(context.Context, *User) error
 		GetByID(context.Context, int64) (*User, error)
-
 	}
 	Comments interface {
 		GetByPostID(context.Context, int64) ([]Comment, error)
 		Create(context.Context, *Comment) error
 	}
+	Followers interface {
+		Follow(ctx context.Context, followerID, userID int64) error
+		Unfollow(ctx context.Context, followerID, userID int64) error
+	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts: &PostStore{db},
-		Users: &UserStore{db},
-		Comments: &CommentsStore{db},
+		Posts:     &PostStore{db},
+		Users:     &UserStore{db},
+		Comments:  &CommentsStore{db},
+		Followers: &FollowerStore{db},
 	}
 }
