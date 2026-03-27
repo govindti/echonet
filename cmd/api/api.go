7539b7f72bb3c1f555550c0bdebd2b6c92caf147
govindti <108tiwari.g@gmail.com>
2026-03-28 01:20:48 +0530

    add index in tables

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -60,7 +60,8 @@ func (app *Application) mount() *chi.Mux {
 				})
 			})
 			r.Route("/users", func(r chi.Router) {
-				r.Route("/{userID}", func(r chi.Router){r.Use(app.usersContextMiddleware)
+				r.Route("/{userID}", func(r chi.Router) {
+					r.Use(app.usersContextMiddleware)
 
 					r.Get("/", app.getUserHandler)
 					r.Put("/follow", app.followUserHandler)
